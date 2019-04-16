debf6b349df624e0f7e733073aad0a1550f8748f
tiago-falves <tiago.falves98@gmail.com>
2019-04-16 09:47:25 +0200

    Nada

--- projA/MyScene.js
+++ projA/MyScene.js
@@ -21,15 +21,11 @@ class MyScene extends CGFscene {
 
         //Initialize scene objects
         this.axis = new CGFaxis(this);
-        this.prism = new MyPrism(this, 4, 4);
-        this.cylinder = new MyCylinder(this, 4, 4);
         this.house = new MyHouse(this);
         //this.tree = new MyTree(this, 2, 1, 2.5, 1);
 
         //Objects connected to MyInterface
         this.displayAxis = true;
-        this.displayMyPrism = true;
-        this.displayMyCylinder = true;
         this.displayMyHouse = true;
         //this.displayMyTree = true;
 
