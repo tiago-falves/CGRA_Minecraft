7b6bba961d0e2445b5920d50d89e385e7ffd4be4
tiago-falves <tiago.falves98@gmail.com>
2019-04-17 20:53:39 +0200

    Comentarios

--- projA/MyInterface.js
+++ projA/MyInterface.js
@@ -16,18 +16,6 @@ class MyInterface extends CGFinterface {
         
         //Checkbox element in GUI
         
-        this.gui.add(this.scene, 'displayAxis').name('Axis');
-        
-        this.gui.add(this.scene, 'displayMyHouse').name('MyHouse');
-                
-        this.gui.add(this.scene, 'displayMyVoxelHill').name('MyVoxelHill');
-        
-        this.gui.add(this.scene, 'displayTreeRowPatch').name('MyTreeRowPatch');
-       
-        this.gui.add(this.scene, 'displayTreeGroupPatch').name('MyTreeGroupPatch');
-        
-        this.gui.add(this.scene, 'displayCubeMap').name('Display CubeMap');
-
         this.gui.add(this.scene, 'lightSelected', this.scene.lightsID).name('Selected Light').onChange(this.scene.chooseLights.bind(this.scene));
 
         this.gui.add(this.scene, 'enableTex').name(' Enable Textures');
